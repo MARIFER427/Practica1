4e23af42d171da2697d8444af58c8c2d992e67a7
MARIFER427 <mariafernanda.demezabermudez.gen23@cbtis105.edu.mx>
2025-03-18 09:38:13 -0600

    Prueba a mongodb y sql

---
+++ CasdenasConexion.cs
@@ -0,0 +1,6 @@
+using Microsoft.Identity.Client;
+
+public class CadenasConexion{
+    public static string SQL_SERVER = "data source=62.146.229.142; user=cbtis105; password=Cbtis105$; TrustServerCertificate=true; initial catalog=Escuela;";
+     public static string MONGO_DB = "mongodb://cbtis105:Cbtis105$@62.146.229.142:27017";
+}
---
+++ Models/CarreraSQL.cs
@@ -0,0 +1,5 @@
+public class CarreraSQL {
+    public short IdCarrera { get; set;}
+
+    public string Carrera { get; set;} = string.Empty;
+}
